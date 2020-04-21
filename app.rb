48fb48123474f8951f76cd1b16d609a0f68923bb
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-04-21 16:23:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = “Jonas Kavaliauskas”
+age = 23
+favorite_food = “burger”
+height_in_inches = 70.8661
+average_male_height_in_inches = 66.9291
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? “You are taller than average.” : “You are not taller than average.”
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
